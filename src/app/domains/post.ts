9b0931386ebfdeb8aa6d753259af4009dccdb537
Hwi Yong Song <hysong15@gmail.com>
2017-11-09 13:26:12 -0800

    Updated explore page

--- src/app/domains/post.ts
+++ src/app/domains/post.ts
@@ -8,16 +8,18 @@ export class Post {
   public urn: string;
   public title: string;
   public body: string;
-  public createdBy: Account;
-  public createdAt: string;
+  public category: string;
   public bounty: Bounty;
   public image: Image;
   public video: Video;
   public organizational: boolean = false;
+  public createdBy: Account;
+  public createdAt: string;
   
   getExcerpt() {
-    if (this.body && this.body.length > 120) {
-      return this.body.substring(0, 120) + "...";
+    if (this.body && this.body.length > 150) {
+      let limit = (this.video == null) ? 150 : 100;
+      return this.body.replace("<br/>", "").substring(0, limit) + "...";
     }
     
     return null;
--- src/app/providers/post.provider.ts
+++ src/app/providers/post.provider.ts
@@ -26,41 +26,17 @@ export class PostProvider {
     return null;
   }
   
-  getPostsByCommunity(communityUrn: string): Array<Post> {
-    if (communityUrn == "urn:community:ai") {
-      return this.getPostsForArtificialIntelligence();
-    }
-    
-    if (communityUrn == "urn:community:ethereum") {
-      return this.getPostsForEthereum(); 
-    }
-    
-    return [];
-  }
-  
   private initMockPosts() {
     this.mockPosts.push(this.createUPortProject());
     this.mockPosts.push(this.createBitAboutBancor());
-    this.mockPosts.push(this.createFacebookArtificialIntelligenceResearch());
-  }
-  
-  private getPostsForEthereum(): Array<Post> {
-    return [
-      this.createUPortProject(),
-      this.createBitAboutBancor()
-    ];
-  }
-  
-  private getPostsForArtificialIntelligence(): Array<Post>{
-    return [
-      this.createFacebookArtificialIntelligenceResearch()
-    ];
+    this.mockPosts.push(this.createAirbnbSurvey());
   }
   
   private createUPortProject(): Post {
     let post = this.createPost("urn:post:uport-project",
-                               "uPort is LIVE. Sign up for our alpha.",
+                               "uPort is LIVE. Join our alpha.",
                                "<p>uPort ID is your mobile identity for secure digital interactions. Download the app to register your self-sovereign identity, collect verifications, log in to applications, and digitally sign Ethereum transactions.</p><p>Our app is now live. Sign up to participate in our alpha test.</p><h4><a href='www.uport.me/signup'>Sign up for Alpha</a></h4>",
+                               "Sign Up",
                                "urn:member:michael-sena",
                                "Nov 5, 2017");
     
@@ -78,13 +54,14 @@ export class PostProvider {
     let post = this.createPost("urn:post:bit-about-bancor",
                                "Introducing Bancor Protocol",
                                "<p>Introducing the Bancor Protocol. Bancor introduces a revolutionary new mechanism to issue tokens, in which one currency can hold one or more other currencies in its reserve. The implications are profound. Much like the Dollar was backed by Gold, new digital currencies can now be backed by Ether, Bitcoin, or any other currency, meaning intrinsically tradable tokens. The White Paper and the Smart Contract are accessible at www.bancor.network.</p>",
+                               "Video",
                                "urn:member:rita-urbach",
                                "Nov 4, 2017");
     
     post.video = this.createVideo("https://www.youtube.com/watch?v=P8EoAvWfFnY",
                                   "A Bit About Bancor",
                                   "The Bancor team shares the vision behind Bancor and a bit about how Smart Tokens work and what they mean for the future of communities.",
-                                  "https://i.ytimg.com/vi_webp/P8EoAvWfFnY/hqdefault.webp",
+                                  "https://img.youtube.com/vi/P8EoAvWfFnY/0.jpg",
                                   "3:44");
     
     post.bounty = this.createBounty(2,
@@ -95,32 +72,28 @@ export class PostProvider {
     return post;
   }
   
-  private createFacebookArtificialIntelligenceResearch(): Post {
-    let post = this.createPost("urn:post:facebook-ai-research",
-                               "Boxes | Let's Unlock Cities",
-                               "<h3>Advancing the field of machine intelligence</h3> <h5>We are committed to advancing the field of machine intelligence and are creating new technologies to give people better ways to communicate. In short, to solve AI.</h5> <p>Facebook Artificial Intelligence Researchers (FAIR) seek to understand and develop systems with human level intelligence by advancing the longer-term academic problems surrounding AI. Our research covers the full spectrum of topics related to AI, and to deriving knowledge from data: theory, algorithms, applications, software infrastructure and hardware infrastructure. Long-term objectives of understanding intelligence and building intelligent machines are bold and ambitious, and we know that making significant progress towards AI can’t be done in isolation. That’s why we actively engage with the research community through publications, open source software, participation in technical conferences and workshops, and collaborations with colleagues in academia.</p> <p>Facebook AI Researchers are based in Menlo Park, New York City and Paris, France.</p> <p>Check us out at <a href='https://research.fb.com/category/facebook-ai-research-fair/'>https://research.fb.com/category/facebook-ai-research-fair/</a>.",
-                               "urn:member:lily-lapcokova",
+  private createAirbnbSurvey(): Post {
+    let post = this.createPost("urn:post:airbnb-survey",
+                               "Give feedback on our new Airbnb homepage experience",
+                               "<h5>Airbnb’s Newly Refreshed and Minimalistic Look</h5> <p>In preparation for the Airbnb Open and the launch of City Hosts, Airbnb is gradually rolling out a new homepage design, with a simpler design and a strong focus on experiences as a key message.</p> <p>As for most of the changes rolled out by Airbnb, the new homepage has been released as an A/B tests and thus is not yet live for 100% of the traffic.</p> <p>To ensure you can view the new version, visit www.airbnb.com in incognito mode or clean your cookies until when you see it.</p> <p>Compared to the old version, the new homepage has a more condensed look, fewer sections and a clearer focus on the “live an experience” message that the company has been pushing more and more as the center of its brand identity.</p> <p>Try out the new homepage, and give us your feedback.</p> <h4><a href='#'>Give Feedback</a></h4>",
+                               "Survey",
+                               "urn:member:kim-kamitani",
                                "Nov 2, 2017");
-    
-    post.video = this.createVideo("https://www.youtube.com/watch?v=-CRJLam3BNc",
-                                  "Facebook's Artificial Intelligence Research",
-                                  "Facebook's Artificial Intelligence team is working to build smart systems that can enhance people's lives. Watch this video to learn about how we're approaching AI research and the impact this work is already having.",
-                                  "https://i.ytimg.com/vi/-CRJLam3BNc/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBB81ZLe0zOGzlhMTVU71cNGk_TWg",
-                                  "3:01");
-    
-    post.bounty = this.createBounty(2,
-                                    98,
-                                    250,
-                                    "VIEW");
+
+     post.bounty = this.createBounty(25,
+                                    72,
+                                    100,
+                                    "SURVEY");
     
     post.organizational = true;
                  
-    return post;
+    return post;                        
   }
   
   private createPost(postUrn: string,
                      title: string,
                      body: string,
+                     category: string,
                      createdBy: string,
                      createdAt: string) {
     let post = new Post();
@@ -128,6 +101,7 @@ export class PostProvider {
     post.urn = postUrn;
     post.title = title;
     post.body = body;
+    post.category = category;
     post.createdBy = this.accountProvider.getAccountByAccountUrn(createdBy);;
     post.createdAt = createdAt;
     
